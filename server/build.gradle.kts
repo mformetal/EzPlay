cd6d6b2384f49a545751b23c1624fe7941d4f39d
Miles Peele <milesbpeele@gmail.com>
2024-01-23 11:18:13 -0500

    Initial commit

---
+++ server/build.gradle.kts
@@ -0,0 +1,34 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.7"
+}
+
+group = "com.ezplay"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.ezplay.ApplicationKt")
+
+    val isDevelopment: Boolean = !project.ext.has("release")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks.create<JavaExec>("runDebug") {
+    
+}
