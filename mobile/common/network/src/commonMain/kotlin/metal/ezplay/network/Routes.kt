1f51469aeeb3b7f47e239a6ee93a572829918bbd
Miles Peele <milesbpeele@gmail.com>
2024-01-31 15:59:40 -0500

    Need to figure out SSL nonsense

--- mobile/common/network/src/commonMain/kotlin/metal/ezplay/network/Routes.kt
+++ mobile/common/network/src/commonMain/kotlin/metal/ezplay/network/Routes.kt
@@ -2,7 +2,7 @@ package metal.ezplay.network
 
 object Routes {
 
-    private const val BASE_URL = "https://10.0.2.2:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     val LIBRARY = BASE_URL.plus("library")
 
