358c23fefbc25142eb43b7796e4a9c18b87dd136
Miles Peele <milesbpeele@gmail.com>
2024-02-01 09:49:45 -0500

    Add preview libs

--- mobile/features/nowplaying/src/androidMain/kotlin/metal/ezplay/nowplaying/NowPlayingScreen.kt
+++ mobile/features/nowplaying/src/androidMain/kotlin/metal/ezplay/nowplaying/NowPlayingScreen.kt
@@ -25,8 +25,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun NowPlayingScreen(nowPlayingViewModel: NowPlayingViewModel) {
